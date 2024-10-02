5824bfa08a59803b0842a46a1b6aa92ab33699eb
Білоцерківець Сергій <sbilocerkivec@gmail.com>
2024-10-02 08:17:54 +0300

    Lesson_15

--- Lesson_15.1.py
+++ Lesson_15.1.py
@@ -26,7 +26,7 @@ class Rectangle:
         return new
 
     def __str__(self):
-        return f"Довжина: {self.width}. Ширина: {self.height}. Площа: {self.get_area()}."
+        return f"Ширина: {self.width}. Висота: {self.height}. Площа: {self.get_area()}."
 
 r1 = Rectangle(2, 4)
 r2 = Rectangle(3, 6)
@@ -34,11 +34,13 @@ assert r1.get_area() == 8, 'Test1'
 assert r2.get_area() == 18, 'Test2'
 
 r3 = r1 + r2
-# print(r3)
+print(r1)
+print(r2)
+print(r3)
 assert r3.get_area() == 26, 'Test3'
 
 r4 = r1 * 4
-# print(r4)
+print(r4)
 assert r4.get_area() == 32, 'Test4'
 
 assert Rectangle(3, 6) == Rectangle(2, 9), 'Test5'
